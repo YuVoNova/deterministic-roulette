eb1625debeb710d4da6baf27d43a362c2369b7f7
Yusuf Metindoğan <yusufmdogan@gmail.com>
2025-03-13 01:23:55 +0300

    feat: static utils class for const values

---
+++ Assets/Modules/Utils/Const.cs
@@ -0,0 +1,9 @@
+namespace Utils
+{
+    public static class Const
+    {
+        public const int MIN_POCKET_VALUE = 0;
+        public const int MAX_POCKET_VALUE = 36;
+        public const string POCKET_TAG = "Pocket";
+    }
+}
