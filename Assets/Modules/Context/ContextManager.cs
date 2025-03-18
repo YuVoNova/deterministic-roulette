6dae8456a35868d70191e1c624bfd5b8477cba78
Yusuf Metindoğan <yusufmdogan@gmail.com>
2025-03-18 22:08:38 +0300

    fix: removing unused services

--- Assets/Modules/Context/ContextManager.cs
+++ Assets/Modules/Context/ContextManager.cs
@@ -5,20 +5,14 @@ namespace Context
     public class ContextManager : IDisposableObject
     {
         public DataStore DataStore => _dataStore;
-        public ILifeCycleCallbacksService LifeCycleCallbacksService => _lifeCycleCallbacksService;
-        public ICoroutineService CoroutineService => _coroutineService;
         public IFileService FileService => _fileService;
 
         private readonly DataStore _dataStore = new();
-        private readonly LifeCycleCallbacksService _lifeCycleCallbacksService = new();
-        private readonly CoroutineService _coroutineService = new();
         private readonly FileService _fileService = new();
 
         public void Dispose()
         {
-            _lifeCycleCallbacksService?.Dispose();
-            _coroutineService?.Dispose();
-            _fileService?.Dispose();
+            _fileService.Dispose();
         }
     }
 }
